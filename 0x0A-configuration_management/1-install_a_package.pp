38411b81a41d07fb801ed5fc7abc2b82867d9f6d
Abdullah <abdallahmosbah25@gmail.com>
2023-12-22 14:32:50 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,21 @@
-# Installs flask, version 2.1.0
+# install_flask.pp
 
-package { 'flask':
+class { 'python':
+  version => '3.8.10',
+}
+
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
