5d21a5ebe915a72e387a11641200a582ee864542
Abdullah <abdallahmosbah25@gmail.com>
2023-12-22 14:38:56 -0800

    try3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,27 +1,5 @@
-# install_flask.pp
+# install puppet-lint -v 2.5.0
 
-class { 'python':
-  version => '3.8.10',
-}
-
-package { 'python3-pip':
-  ensure => present,
-}
-
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
-}
-
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
-}
-
-exec { 'Fix URI.escape warning':
-  command     => 'sed -i "s/URI.escape/URI::DEFAULT_PARSER.escape/" /path/to/your/puppet/modules',
-  refreshonly => true,
-  subscribe   => Package['python3-pip'],
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
