41d254366c03ec175158ec6d48e44a047755856e
Abdullah <abdallahmosbah25@gmail.com>
2023-12-22 14:55:19 -0800

    dD

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,7 @@
-# install_flask.pp
-
+# Install flask using puppet
 package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
-}
-
+    ensure   => '2.1.0',
+    provider => 'pip3', }
 package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-}
+    ensure   => '2.1.1',
+    provider => 'pip3', }
