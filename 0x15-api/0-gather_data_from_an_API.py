3b90c5ed5c398bd075edfa7c6ef288a6b6939681
Abdullah <abdallahmosbah25@gmail.com>
2024-01-30 01:49:19 +0200

    documented

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-""" get data from api """
+"""
+get data from api for the given
+emp id the script should get the name
+and the completed task
+"""
 
 import requests
 import sys
 
-url = "https://jsonplaceholder.typicode.com/"
+if __name__ == "__main__":
 
+    url = "https://jsonplaceholder.typicode.com/"
 
-emp_todo = requests.get(f"{url}users/{sys.argv[1]}/todos")
-emp_name = requests.get(f"{url}users/{sys.argv[1]}")
+    emp_todo = requests.get(f"{url}users/{sys.argv[1]}/todos")
+    emp_name = requests.get(f"{url}users/{sys.argv[1]}")
 
+    emp_todo = emp_todo.json()
+    emp_name = emp_name.json()['name']
 
-emp_todo = emp_todo.json()
-emp_name = emp_name.json()['name']
+    total_tasks = 0
 
-total_tasks = 0
+    for task in emp_todo:
+        if task['completed']:
+            total_tasks += 1
 
-for task in emp_todo:
-    if task['completed']:
-        total_tasks += 1
+    print(f"Employee {emp_name} is done with\
+          tasks({total_tasks}/{len(emp_todo)})")
 
-print(f"Employee {emp_name} is done with tasks({total_tasks}/{len(emp_todo)})")
-
-for line in emp_todo:
-    if line['completed']:
-        print(f"\t{line['title']}")
+    for line in emp_todo:
+        if line['completed']:
+            print(f"\t{line['title']}")
