3d5ffba1300f97c9bbf4d3ff74b0661dc4e1fb47
Abdullah <abdallahmosbah25@gmail.com>
2024-01-30 01:59:06 +0200

    documented v3

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -24,8 +24,9 @@ if __name__ == "__main__":
         if task['completed']:
             total_tasks += 1
 
-    print(f"Employee {emp_name} is done with tasks({total_tasks}/{len(emp_todo)}):")
+    print(f"Employee {emp_name} is done with tasks\
+    ({total_tasks}/{len(emp_todo)}):")
 
     for line in emp_todo:
         if line['completed']:
-            print(f"\t{line['title']}")
+            print(f"\t {line['title']}")
