1ed88b15f57943fa6694ce4cfacf9410af1a0fcc
Abdullah <abdallahmosbah25@gmail.com>
2024-02-14 06:37:51 +0200

    Task1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""get subs"""
+""" get subscribers"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """get the number of reddit subs"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    res = requests.get(url, headers=header, allow_redirects=False)
-    if res.status_code == 404:
+    """Get subs number"""
+
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'AyaTarek95'}
+    res = requests.get(url, headers=headers)
+
+    if res.status_code == 200:
+        data = res.json()
+        subscribers_num = data['data']['subscribers']
+        return subscribers_num
+
+    else:
         return 0
-    result = res.json().get("data")
-    return result.get("subscribers")
