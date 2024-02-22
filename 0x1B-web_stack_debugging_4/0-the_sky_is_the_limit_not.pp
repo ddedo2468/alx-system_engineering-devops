15e492e2b07d32dd24761dbda795e24a0541c6fa
Abdullah <abdallahmosbah25@gmail.com>
2024-02-22 08:02:06 +0200

    puppets

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+exec { 'ulimit fix':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+exec { 'restart nginx':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
+
